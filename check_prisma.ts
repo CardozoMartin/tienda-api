99979bf200a9cd85c26d9c780c8ea99c755592ea
Martin Cardozo <martin.cardozo@zymo.it>
2026-04-10 00:37:11 -0300

    se quitan algunos archivos que no se usan mas

--- check_prisma.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from './src/config/prisma';
-async function main() {
-  console.log('Prisma keys:', Object.keys(prisma));
-  console.log('Prisma.carrito:', prisma.carrito);
-}
-main().catch(console.error);
--- list_stores.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const tiendas = await prisma.tienda.findMany({
-    select: { slug: true, nombre: true, plantillaId: true }
-  });
-  console.log(JSON.stringify(tiendas, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
